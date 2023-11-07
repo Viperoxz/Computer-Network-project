824acaba2730c25300ae33ee7b126df194804dae
Viperoxz <pvhn191004@gmail.com>
2023-11-07 22:35:41 +0700

    Merge remote-tracking branch 'origin/main'

--- src/main/java/testsocket/clientTCP.java
+++ src/main/java/testsocket/clientTCP.java
@@ -2,18 +2,39 @@ package testsocket;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
+import javax.mail.internet.InternetAddress;
+
+class CheckMail extends Thread{
+    String host = "pop.gmail.com";
+    String storeType = "pop3";
+    String username = "pvhoangnamzz@gmail.com";
+    String appPassword = "drzd dpmu evff ejqj";
+    String sender;
+    ReceiveMail mailReceived;
+
+//    public CheckMail(String sender){
+//        this.sender = sender;
+//        this.mailReceived = new ReceiveMail(host, storeType, username, appPassword);
+//    }
+//    public boolean check(Pair<String, String> newMail, List<Pair<String, String>> oldMail){
+//        return true;
+//    }
+}
+
 public class clientTCP {
-    public static void main(String argv[]) throws Exception
-    {
+    public static void main(String argv[]) throws Exception {
         String sentence_to_server;
         String sentence_from_server;
 
-        //Tạo Inputstream(từ bàn phím)
-        System.out.print("Input from client: ");
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        //Lấy chuỗi ký tự nhập từ bàn phím
-        sentence_to_server = inFromUser.readLine();
-//
         //Tạo socket cho client kết nối đến server qua ID address và port number
         Socket clientSocket = new Socket("127.0.0.1", 6543);
 
@@ -22,17 +43,31 @@ public class clientTCP {
 
         //Tạo inputStream nối với Socket
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-//
-        //Gửi chuỗi ký tự tới Server thông qua outputStream đã nối với Socket (ở trên)
-        outToServer.writeBytes(sentence_to_server + '\n');
 
-        //Đọc tin từ Server thông qua InputSteam đã nối với socket
-        sentence_from_server = inFromServer.readLine();
+        // Tạo input stream từ bàn phím
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-        //print kết qua ra màn hình
-        System.out.println("FROM SERVER: Ban " + sentence_from_server);
+        while (true) {
+            // Lấy chuỗi ký tự nhập từ bàn phím
+            System.out.print("Input from client: ");
+            sentence_to_server = inFromUser.readLine();
 
-        //Đóng liên kết socket
+            // Gửi chuỗi ký tự tới Server thông qua outputStream đã nối với Socket (ở trên)
+            outToServer.writeBytes(sentence_to_server + '\n');
+
+            // Đọc tin từ Server thông qua InputSteam đã nối với socket
+            sentence_from_server = inFromServer.readLine();
+
+            // In kết quả ra màn hình
+            System.out.println("FROM SERVER: " + sentence_from_server);
+
+            // Kiểm tra nếu client nhập "quit" thì kết thúc vòng lặp
+            if ("quit".equals(sentence_to_server)) {
+                break;
+            }
+        }
+
+        // Đóng liên kết socket
         clientSocket.close();
     }
 }
--- src/main/java/testsocket/serverTCP.java
+++ src/main/java/testsocket/serverTCP.java
@@ -6,34 +6,37 @@ import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
 public class serverTCP {
-    public static void main(String argv[]) throws Exception
-    {
+    public static void main(String argv[]) throws Exception {
         String sentence_from_client;
         String sentence_to_client;
 
-        //Tạo socket server, chờ tại cổng '6543'
+        // Tạo socket server, chờ tại cổng '6543'
         ServerSocket welcomeSocket = new ServerSocket(6543);
 
         while (true) {
-            //chờ yêu cầu từ client
+            // Chờ yêu cầu từ client
             Socket connectionSocket = welcomeSocket.accept();
 
-            //Tạo input stream, nối tới Socket
-            BufferedReader inFromClient =
-                    new BufferedReader(new
-                            InputStreamReader(connectionSocket.getInputStream()));
+            // Tạo input stream, nối tới Socket
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 
-            //Tạo outputStream, nối tới socket
-            DataOutputStream outToClient =
-                    new DataOutputStream(connectionSocket.getOutputStream());
+            // Tạo outputStream, nối tới socket
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-            //Đọc thông tin từ socket
-            sentence_from_client = inFromClient.readLine();
+            while (true) {
+                // Đọc thông tin từ socket
+                sentence_from_client = inFromClient.readLine();
 
-            sentence_to_client = sentence_from_client +" (Server accepted!)" + '\n';
-            //ghi dữ liệu ra socket
-            outToClient.writeBytes(sentence_to_client);
-            return;
+                if ("quit".equals(sentence_from_client)) {
+                    // Đóng kết nối với client hiện tại
+                    connectionSocket.close();
+                    return; // Thoát khỏi vòng lặp và chờ kết nối mới
+                }
+
+                sentence_to_client = sentence_from_client + " (Server accepted!)" + '\n';
+                // Ghi dữ liệu ra socket
+                outToClient.writeBytes(sentence_to_client);
+            }
         }
     }
 }
