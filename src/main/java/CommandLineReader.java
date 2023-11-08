2d36fa65cbd1b1f403ad41743931f9735966ef1b
Đạt Trương <120260691+tiendat25052004@users.noreply.github.com>
2023-11-08 21:32:27 +0700

    Add files via upload

---
+++ src/main/java/CommandLineReader.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.awt.AWTException;
+
+public class CommandLineReader {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập lệnh: ");
+        String command = scanner.nextLine();
+
+        switch (command.toUpperCase()) {
+            case "SHUTDOWN":
+                System.out.println("Tắt máy tính...");
+                new Shutdown();
+                break;
+            case "SCREENSHOT":
+                try {
+                    ScreenCapture.captureScreen();
+                } catch (IOException | AWTException ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            case "TAKEKEY":
+                new KeyCapture();
+                break;
+            case "PROCESS":
+                System.out.println("Danh sách các tiến trình:");
+                new ProcessManager().listProcesses();
+                // Mở process nè
+                System.out.println("Bạn muốn mở?");
+                command = scanner.nextLine();
+                new ProcessManager().startProcess(command);
+                // Đóng process nè
+                System.out.println("Bạn muốn đóng?");
+                command = scanner.nextLine();
+                new ProcessManager().stopProcess(command);
+                break;
+            case "APP":
+
+            default:
+                System.out.println("Lệnh không hợp lệ.");
+        }
+    }
+}
