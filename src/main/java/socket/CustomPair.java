081d257af919bb280b647f8d5b79d3637ac15df3
Viperoxz <pvhn191004@gmail.com>
2023-12-06 12:20:38 +0700

    move custompair to new file

---
+++ src/main/java/socket/CustomPair.java
@@ -0,0 +1,24 @@
+package socket;
+
+public class CustomPair<K, V> {
+    private K key;
+    private V value;
+
+    private CustomPair(){}
+
+    public CustomPair(K k, V v) {
+        this.key = k;
+        this.value = v;
+    }
+    public static <K, V> CustomPair of (K key, V value) {
+        return new CustomPair<>(key, value);
+    }
+
+    public K getKey() {
+        return this.key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
